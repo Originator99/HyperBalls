98c4108ccd531484506d55f558b51e7f0527f48e
Originator99 <arsalaan.md0077@gmail.com>
2020-07-20 13:54:20 +0530

    walls top and bottom removed

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -14,9 +14,13 @@ public class PlayerMotor : MonoBehaviour {
 	private bool isGrounded;
 	private Rigidbody2D rb;
 
+	private float verticalExt;
+
 	private void Start() {
 		isGrounded = false;
 		rb = GetComponent<Rigidbody2D>();
+		verticalExt = Camera.main.orthographicSize;
+		Debug.Log("Vertical : " + verticalExt);
 	}
 
 	private void Update() {
@@ -49,5 +53,17 @@ public class PlayerMotor : MonoBehaviour {
 #else
 		transform.position += new Vector3(0, movementArea.GetTouchPosition.y, 0f) * MoveSpeed * Time.unscaledDeltaTime;
 #endif
+
+		UpdatePlayerYPosition();
+	}
+
+	private void UpdatePlayerYPosition() {
+		if (verticalExt != 0) {
+			if (transform.position.y > verticalExt) {
+				transform.position = new Vector2(transform.position.x, verticalExt * -1);
+			} else if (transform.position.y < -verticalExt) {
+				transform.position = new Vector2(transform.position.x, verticalExt);
+			}
+		}
 	}
 }
